d4fac53bc3e2151f03b5dd9de36a5838b6833e9a
Starrpoek <thomas.peoch@gmail.com>
2025-03-24 15:15:30 +0100

    MaJ site web dynamique
    Ajout de détails et du lien de la plateforme GoFundMe

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const el = entry.target;
+        if (entry.isIntersecting) {
+          el.classList.add("visible");
+        } else {
+          el.classList.remove("visible");
+        }
+      });
+    }, { threshold: 0.5 }); // 50% visible
+  
+    const target = document.querySelector(".highlight-42");
+    if (target) observer.observe(target);
+  });
+  
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
